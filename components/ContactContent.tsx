75d9012035e3b893edcf8180b869b6d61478dd9d
anhphgameestudio <89737507+hoanganhphan0409@users.noreply.github.com>
2024-11-06 09:48:39 +0700

    Update ContactContent.tsx

--- components/ContactContent.tsx
+++ components/ContactContent.tsx
@@ -18,8 +18,6 @@ const ContactContent = () => {
         const response = await fetch("http://localhost:8888/friends");
         if (!response.ok) throw new Error("Failed to fetch friends");
         const data = await response.json();
-        console.log(data);
-
         setMembers(data);
       } catch (error) {
         console.error("Error fetching members:", error);
